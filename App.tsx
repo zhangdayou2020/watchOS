b4d90045bcca05f50ff961e21ec7d1236349e095
Link <66194767+zhangdayou2020@users.noreply.github.com>
2025-06-17 11:30:08 +0800

    数据的同步化存储

--- App.tsx
+++ App.tsx
@@ -1,24 +1,44 @@
 import * as React from 'react';
+import {useEffect} from 'react';
 import LoginScreen from '@/screens/LoginScreen';
 import HomeScreen from '@/screens/HomeScreen';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Provider} from 'react-redux';
+import {Provider, useDispatch} from 'react-redux';
 import store from '@/store';
+import {getUserFromStorage} from '@/utils/storage';
+import {setUserInfo} from '@/store/userSlice';
 
 const Stack = createNativeStackNavigator();
+
+function AppContent() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // 启动时自动同步本地用户信息到 Redux
+    getUserFromStorage().then(user => {
+      if (user) {
+        dispatch(setUserInfo(user));
+      }
+    });
+  }, [dispatch]);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Login"
-          screenOptions={{headerShown: false}}>
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Home" component={HomeScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <AppContent />
     </Provider>
   );
 }
